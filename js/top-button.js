79b80fc2492656f10e82372213a452a509cb1910
Đặng Minh Thắng <thang2k2la@gmail.com>
2024-08-12 16:38:53 +0700

    fix layout

---
+++ js/top-button.js
@@ -0,0 +1,23 @@
+// Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document with animation
+scrollToTopBtn.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // This enables the smooth scrolling animation
+    });
+};
